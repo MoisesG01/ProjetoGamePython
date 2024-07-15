8587219cb5fd5152e24807dc1e7b1871438bfbad
Moises Goncalves <moises_goncalves@outlook.com.br>
2024-07-14 21:02:38 -0300

    Primeiro Commit

--- Game/pythonGame/code/Game.py
+++ Game/pythonGame/code/Game.py
@@ -1,8 +1,11 @@
+import sys
+
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 from code.Menu import Menu
 
 
@@ -15,4 +18,11 @@ class Game:
 
         while True:
             menu = Menu(self.window)
-            menu.run()
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            else:
+                pygame.quit()
+                sys.exit()
